2ee685a08242ee15808d3ba580b8840e7c4e44d9
Ammad <ammadq87@my.yorku.ca>
2023-04-09 23:45:18 -0400

    fix up

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express();
-const loginDAO = require('../dao/loginDAO');
+const _loginDAO = require('../dao/loginDAO');
 
 router.get('/', async (req, res) => {
     const data = req.body;
     try {
+        const loginDAO = new _loginDAO.loginDAO(); 
         const validLogin = await loginDAO.validateLogin(data);
         res.send(validLogin ? `Welcome, ${data['email']}!` : 'Invalid Username/Password Combo');
     } catch (err) {
